9ba1cd243862530b261bb5548cd61dae1ba0911f
羊羊 <flexinruss@foxmail.com>
2020-02-27 23:48:04 +0800

    init

---
+++ LoginModule/LoginModule.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace MySelfProject
+{
+    public class LoginModule
+    {
+        static string loginBaseUrl = "http://60.205.205.201";
+        static string RestryBaseUrl = "http://60.205.205.201:8001/login";
+        UserToken token;
+        public LoginModule(string username, string password)
+        {
+            token = new UserToken();
+            token.UserName = username;
+            token.Password = password;
+            init();
+        }
+        public string result = string.Empty;
+
+        async void init()
+        {
+            result = await Restry();
+        }
+
+        private string Base64Encrypt(string txt)
+        {
+            return txt;
+        }
+
+        private string EnCryPt(string txt)
+        {
+            return txt;
+        }
+
+        private async Task<string> Restry()
+        {
+            var body = MakeJosnData();
+            var res = await PostData(body);
+            using (StreamReader reader = new StreamReader(res.GetResponseStream(), Encoding.UTF8))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        private async Task<WebResponse> PostData(string body)
+        {
+            Encoding encode = Encoding.UTF8;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.CreateHttp(RestryBaseUrl);
+            request.Method = "POST";
+            request.Accept = "text/html, application/xhtml+xml, */*";
+            request.ContentType = "application/json";
+            byte[] buffer = encode.GetBytes(body);
+            request.ContentLength = buffer.Length;
+            request.GetRequestStream().Write(buffer, 0, buffer.Length);
+            var response = await request.GetResponseAsync();
+            return response;
+        }
+
+        private string MakeJosnData()
+        {
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            dict["username"] = Base64Encrypt(token.UserName); ;
+            dict["password"] = EnCryPt(token.Password);
+            return JsonConvert.SerializeObject(dict);
+        }
+
+    }
+
+    public class UserToken
+    {
+        private string _username;
+        public string UserName
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+            }
+        }
+
+        private string _password;
+
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+            }
+        }
+    }
+
+
+    public class RegistyBody
+    {
+        private string _username;
+        public string UserName
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    IsBad = true;
+                }
+                _username = value;
+            }
+        }
+
+        public bool IsBad = false;
+
+        private string _password;
+
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    IsBad = true;
+                }
+                _password = value;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MySelfProject
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double windowWid = 700;
+        double windowHei = 400;
+        double TextHeight = 20;
+        double TextWidth = 80;
+        DockPanel mainDock = new DockPanel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ResizeMode = ResizeMode.NoResize;
+            Width = windowWid;
+            Height = windowHei;
+            CreateDeafautLayout();
+            Content = mainDock;
+        }
+
+        void CreateDeafautLayout()
+        {
+            Image img = new Image() { Width = windowWid / 2, Height = windowHei};
+            img.Source = new BitmapImage(new Uri("/images/huang.jpg", UriKind.Relative)); ;
+            mainDock.Children.Add(img);
+            DockPanel.SetDock(img, Dock.Left);
+            TextBox tx = new TextBox() { Width = TextWidth, Height = TextHeight };
+            mainDock.Children.Add(tx);
+            DockPanel.SetDock(tx, Dock.Top);
+
+            TextBox tx2 = new TextBox() { Width = TextWidth, Height = TextHeight };
+            mainDock.Children.Add(tx2);
+            DockPanel.SetDock(tx2, Dock.Top);
+
+            Button Submit = new Button();
+            Submit.Tag = "确认";
+            mainDock.Children.Add(Submit);
+            DockPanel.SetDock(Submit, Dock.Bottom);
+
+
+            Label response = new Label() {Width = 20, Height = 20, Background = new SolidColorBrush(Colors.Pink) };
+            mainDock.Children.Add(response);
+            DockPanel.SetDock(response, Dock.Top);
+
+            string username = tx.Text;
+            string passwod = tx2.Text;
+            var x = new LoginModule(username, passwod);
+            Submit.Click += (sender, e) =>
+            {
+                //Application.Current.Dispatcher.Invoke(new Action(() => 
+                //{
+                response.Content = x.result;
+                //}));
+            };
+
+        }
+
+    }
+}
